55d816758d928e2aa85d9590a57f9a9b86693f18
Abdali21 <160420279+Abdali21@users.noreply.github.com>
2025-02-24 18:31:30 +0000

    setup publication

--- routes/web.php
+++ routes/web.php
@@ -1,36 +1,17 @@
 <?php
+
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+// profile side ---------------------------------------------------------------------------------------------------------------------------
+Route::resource("profiles", ProfileController::class);
 
-// all profiles --------------------------------------------------------------------------------------------------------------------
-Route::get("/profiles", [ProfileController::class, "index"])->name("profiles.index");
-
-
-// insertion-------------------------------------------------------------------------------------------------------------------------------
-Route::get("/profiles/create", [ProfileController::class, "create"])->name("profiles.create");
-Route::post("/profiles", [ProfileController::class, "store"])->name("profiles.store");
-
-// delete-----------------------------------------------------------------------------------------------------------------------------------
-Route::delete("/profiles/{profile}", [ProfileController::class, "destroy"])->name("profiles.destroy");
-
-// update-----------------------------------------------------------------------------------------------------------------------------------
-Route::get("/profiles/{profile}/edit", [ProfileController::class, "edit"])->name("profiles.edit");
-Route::put("/profiles/{profile}", [ProfileController::class, "update"])->name("profiles.update"); 
-
-// show------------------------------------------------------------------------------------------------------------------------------------
-Route::get("/profiles/{profile}", [ProfileController::class, "show"])
-->where("profile", "\d+")	
-->name("profiles.show");
-
-
-// login side -----------------------------------------------------------------------------------------------------------------------
+// login side -----------------------------------------------------------------------------------------------------------------------------
 Route::get("/login", [LoginController::class, "show"])->name("login.show");
 Route::post("/login", [LoginController::class, "login"])->name("login.login");
-Route::get("/logout",[LoginController::class, "logout"])->name("login.logout");
-
+Route::get("/logout", [LoginController::class, "logout"])->name("login.logout");
 
 // home -----------------------------------------------------------------------------------------------------------------------------------
-Route::get("/",[HomeController::class, "index"])->name(("home"));
+Route::get("/", [HomeController::class, "index"])->name(("home"));
